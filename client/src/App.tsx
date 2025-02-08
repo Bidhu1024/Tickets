d54988bdca7dbcc5b1fad106ca557f9ae0fe5034
Bidhu1024 <bidhu1024av@gmail.com>
2025-02-09 01:11:51 +0530

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { Suspense } from "react";
+import LoadingComponent from "./helpers/LoadingComponent";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import React from 'react';
-import {Suspense} from 'react';
-import LoadingComponent from './helpers/LoadingComponent';
-
-const Login = React.lazy(() => import('./screens/Login/Login'));
+const Login = React.lazy(() => import("./screens/Login/Login"));
 
 function App() {
-  
-
   return (
-<BrowserRouter>
-<Suspense fallback={<LoadingComponent/>}>
-<Routes>
- 
-<Route path="/" element={<Login/>}/>
-
-</Routes>
-</Suspense>
-</BrowserRouter>
-  )
+    <BrowserRouter>
+      <Suspense fallback={<LoadingComponent />}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
