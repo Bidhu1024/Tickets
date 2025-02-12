d97fc186d12be164d14d756dcfb121bdaedd5887
Bidhu1024 <bidhu1024av@gmail.com>
2025-02-12 11:00:52 +0530

    App not starting

---
+++ server/src/controllers/auth.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express';
+import User from '../Models/User';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key';
+
+// @desc    Register a new user
+// @route   POST /api/auth/register
+// @access  Public
+export const registerUser = async (req: Request, res: Response): Promise<void> => {
+  const { name, email, password, phone, confirmPassword } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ message: 'User already exists' });
+      return;
+    }
+
+    const user = await User.create({ name, email, password, phone, confirmPassword });
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.status(201).json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: 'Invalid credentials' });
+      return;
+    }
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      res.status(400).json({ message: 'Invalid credentials' });
+      return;
+    }
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
---
+++ server/src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { Express, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import authRoutes from "./routes/Auth";
+
+mongoose.set("strictQuery", true);
+dotenv.config();
+const app: Express = express();
+
+const connect = async () => {
+  try {
+    const mongo = process.env.MONGO_URI;
+    if (!mongo) return;
+    await mongoose.connect(mongo);
+    console.log("connected to mongodb");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongodb disconnected");
+});
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", authRoutes);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(Error("Endpoint not found"));
+});
+
+app.listen(8800, () => {
+  console.log("server started at port 8800");
+  connect();
+  console.log("connected to backend");
+});
