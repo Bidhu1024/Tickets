3da8de1c9e4648c700d7c6c647d8d6c5b8196349
Bidhu1024 <bidhu1024av@gmail.com>
2025-04-11 22:31:26 +0530

    some changes done

---
+++ client/src/components/svg/Yellow.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const SoldYellowCircle = () => (
+  <svg width="15" height="15" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="25" cy="25" r="25" fill="#FFD700" />
+   
+  </svg>
+);
